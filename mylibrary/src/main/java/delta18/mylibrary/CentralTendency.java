5f42089ae5338dae0fa4a1a008fbf3f70135a56c
Sivasankari S <sivasankari@Sivasankaris-MacBook-Air.local>
2017-05-19 23:43:51 +0530

    first commit

---
+++ mylibrary/src/main/java/delta18/mylibrary/CentralTendency.java
@@ -0,0 +1,14 @@
+package delta18.mylibrary;
+
+import android.content.Context;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CentralTendency {
+    private String Value;
+    public CentralTendency(Context context){
+
+    }
+
+}
